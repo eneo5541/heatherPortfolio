66028cf728d3ca845eee000ca9fca35a46dc40c4
Eric <neo_eric@hotmail.com>
2015-08-09 12:41:53 +1000

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,50 @@
+var menuRactive = getMenu();
+function getMenu ()
+{
+	Ractive.load('./pages/menu.html').then(function (Menu) {
+		menuRactive = new Menu({
+			el: 'menu',
+			data: { }
+		});
+		
+		$.getJSON( "./assets/json/menu.json", function(data) {
+			console.log("Setting data");
+			menuRactive.set('list', data);
+			init();
+		});
+		
+		return menuRactive;
+	});
+}
+
+var r = Rlite();
+
+function processHash()
+ {
+	var hash = location.hash || '#';
+	r.run(hash.slice(1));
+}
+function init()
+{
+	r.add('', function () {
+		window.location = "#page/home"
+	});
+	
+	r.add('page/:id', openPage);
+	
+	window.addEventListener('hashchange', processHash); 
+	processHash();
+}
+
+var pageRactive;
+function openPage(pageId) 
+{
+	Ractive.load('./pages/' + pageId.params.id + '.html').then( function (Page) {
+		pageRactive = new Page({
+			el: 'content',
+			data: { }
+		});
+    });
+}
+
+
